43d445cdf62905657f7e6c1b99b7f4443035c579
sefiliss <ielizabeth19732@gmail.com>
2024-12-03 00:20:55 +0300

    no message

--- main.c
+++ main.c
@@ -1,4 +1,28 @@
-﻿int main(void)
+﻿#include <stdio.h>
+#include<stdlib.h>
+#include <math.h>
+#include <float.h>
+
+/**
+ * @brief считывает целое число
+ * @return целое число
+ */
+int DInput(void);
+
+/**
+ * @brief считывает целое число и проверяет знак
+ * @return целое положительное число
+ */
+int DPositiveInput(void);
+
+
+
+/**
+ * @brief точка входа в программу
+ * @return 0 в случае успеха
+ */
+int main(void)
 {
-    
+    puts("Введите количество элементов n массива:");
+    const int n = DPositiveInput();
 }
