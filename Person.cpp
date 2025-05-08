a540b22332b5864b2311b0cc505774efc7d95113
sefiliss <ielizabeth19732@gmail.com>
2025-05-08 17:17:03 +0300

    задание 1

---
+++ Person.cpp
@@ -0,0 +1,23 @@
+﻿#include "Person.h"
+
+Person::Person():
+  lastName{""}, firstName{""}, patronymic{""}
+{
+}
+
+Person::Person(const string& ln, const string& fn, const string& pn)
+{
+  lastName = ln;
+  firstName = fn;
+  patronymic = pn;
+}
+
+void Person::Show()
+{
+  cout << lastName << " " << firstName << endl;
+}
+
+void Person::ShowFormal()
+{
+  cout << lastName << " " << firstName << " " << patronymic << endl;
+}
---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Person.h"
+
+using namespace std;
+
+int main(void) {
+  string ln, fn, pn;
+  cout << "Введите имя:";
+  cin >> ln;
+  cout << "Введите фамилию:";
+  cin >> fn;
+  cout << "Введите отчество:";
+  cin >> pn;
+  Person student(ln, fn, pn);
+  student.Show();
+  student.ShowFormal();
+  return 0;
+}
