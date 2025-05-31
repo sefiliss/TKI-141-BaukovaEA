9a6e7adaf8e3ead7569b081521c5e7895507809a
sefiliss <126054068+sefiliss@users.noreply.github.com>
2025-05-31 09:05:06 +0300

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,3 +1,8 @@
 #include "Node.h"
 
+/**
+ * @brief Конструктор узла
+ * @param val Значение узла
+ * @param nxt Указатель на следующий узел
+ */
 Node::Node(int val, Node* nxt) : value(val), next(nxt) {}
